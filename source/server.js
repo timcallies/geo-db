f68b8b0c04ea301a761d30d3690f5a09ba005c1f
SupahAmbition <tylerbeverley@gmail.com>
2019-03-30 16:13:15 -0500

    Created generic query function

--- source/server.js
+++ source/server.js
@@ -1,3 +1,7 @@
+var stdin = process.stdin; 
+stdin.setEncoding('utf-8'); 
+
+
 var express = require('express');
 var app = express();
 
@@ -6,10 +10,71 @@ var connection = mysql.createConnection({
     host        : 'localhost',
     user        : 'me',
     password    : '',
-    database    : 'test'
+    database    : 'geodb'
 });
 
 connection.connect();
+
+
+
+
+
+
+
+
+
+
+
+
+/* ===============WARNING=================
+ *
+ *
+ * This function is meant only for dev purposes. 
+ * Use this function if you mess up the database
+ * and want to restore it back to it's original state. 
+ * This function will ask for confirmation on the command line. 
+ * 
+ */
+function restoreFromCSV( )
+{
+
+	console.log("Are you sure you want to restore geodb? (yes / no"); 
+	stdin.on('data', function(data)  {
+
+		if(data === 'yes'){
+			//remove and re-add geodb.
+			
+			//load the csv files into geodb. 
+
+		}
+	}); 
+};
+
+
+
+/* This is a general purpose helper function 
+ * for sending queries to the geodb using the 'me'
+ * user.  This function does not sanatize sql input. 
+ */
+function sendQuery( queryString, callback ){
+
+	function queryExec(resolve, reject) {
+
+		connection.query( queryString, 	function( error, results) {
+				if(error) return reject(error); 
+				else { 
+					console.log('fulfilled query promise'); 
+					resolve(results); 
+				}
+			});
+	};
+	
+	return new Promise( queryExec ); 
+};
+
+
+
+
 
 
 app.get('/', function(req, res){
